ae109da579c35f3a844de98884c4fe7f3ff1638f
Monty Carlo Pineda <monty.carlo.pineda@codeandtheory.com>
2022-10-29 05:17:01 +0800

    Add the completed RSVP web app

--- rct-401-react-learn-more/rsvp/src/App.js
+++ rct-401-react-learn-more/rsvp/src/App.js
@@ -1,13 +1,117 @@
+import React, { useState } from 'react';
 import './App.css';
 
+import GuestList from './components/GuestList';
+import Counter from './components/Counter';
+
 function App() {
+  const [guests, setGuests] = useState(
+    [
+      {
+        name: 'Treasure',
+        isConfirmed: false,
+        isEditing: false
+      },
+      {
+        name: 'Nic',
+        isConfirmed: true,
+        isEditing: false
+      },
+      {
+        name: 'Matt K',
+        isConfirmed: false,
+        isEditing: true
+      }
+    ]
+  );
+
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  const [pendingGuest, setPendingGuest] = useState("");
+
+  const toggleGuestPropertyAt = (property, indexToChange) => 
+    setGuests(
+      guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            [property]: !guest[property]
+          };
+        } else {
+          return guest;
+        }
+      })
+    );
+
+  const toggleConfirmationAt = index =>
+    toggleGuestPropertyAt('isConfirmed', index);
+
+  const removeGuestAt = index => 
+    setGuests([
+      ...guests.slice(0, index),
+      ...guests.slice(index + 1),
+    ]);
+  
+  const toggleEditingAt = index =>
+    toggleGuestPropertyAt('isEditing', index);
+
+  const setNameAt = (name, indexToChange) => 
+    setGuests(
+      guests.map((guest, index) => {
+        if (index === indexToChange) {
+          return {
+            ...guest,
+            name
+          };
+        } else {
+          return guest;
+        }
+      })
+    );
+
+  const toggleFilter = () => 
+    setIsFiltered(!isFiltered);
+
+  const handleNameInput = event => 
+    setPendingGuest(event.target.value);
+
+  const newGuestSubmitHandler = event => {
+    event.preventDefault();
+    setGuests([
+      {
+        name: pendingGuest,
+        isConfirmed: false,
+        isEditing: false
+      },
+      ...guests
+    ]);
+    setPendingGuest('');
+  };
+
+  const getTotalInvited = () => 
+    guests.length;
+
+  const getAttendingGuest = () => 
+    guests.reduce((total, guest) => 
+      guest.isConfirmed ? total + 1 : total, 
+      0
+    );
+
+  const totalInvited = getTotalInvited();
+  const numberAttending = getAttendingGuest();
+  const numberUnconfirmed = totalInvited - numberAttending;
+
   return (
     <div className="App">
-      <header>
+      <header> 
         <h1>RSVP</h1>
         <p>A Treehouse App</p>
-        <form>
-            <input type="text" value="Safia" placeholder="Invite Someone" />
+        <form onSubmit={newGuestSubmitHandler}>
+            <input 
+              type="text" 
+              onChange={handleNameInput}
+              value={pendingGuest}
+              placeholder="Invite Someone" />
             <button type="submit" name="submit" value="submit">Submit</button>
         </form>
       </header>
@@ -15,51 +119,24 @@ function App() {
         <div>
           <h2>Invitees</h2>
           <label>
-            <input type="checkbox" /> Hide those who haven't responded
+            <input 
+              type="checkbox"
+              onChange={toggleFilter}
+              checked={isFiltered} /> Hide those who haven't responded
           </label>
         </div>
-        <table className="counter">
-          <tbody>
-            <tr>
-              <td>Attending:</td>
-              <td>2</td>
-            </tr>
-            <tr>
-              <td>Unconfirmed:</td>
-              <td>1</td>
-            </tr>
-            <tr>
-              <td>Total:</td>
-              <td>3</td>
-            </tr>
-          </tbody>
-        </table>
-        <ul>
-          <li className="pending"><span>Safia</span></li>
-          <li className="responded"><span>Iver</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li className="responded">
-            <span>Corrina</span>
-            <label>
-              <input type="checkbox" checked /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-          <li>
-            <span>Joel</span>
-            <label>
-              <input type="checkbox" /> Confirmed
-            </label>
-            <button>edit</button>
-            <button>remove</button>
-          </li>
-        </ul>
+        <Counter 
+          totalInvited={totalInvited}
+          numberAttending={numberAttending}
+          numberUnconfirmed={numberUnconfirmed} />
+        <GuestList 
+          guests={guests}
+          toggleConfirmationAt={toggleConfirmationAt}
+          toggleEditingAt={toggleEditingAt}
+          setNameAt={setNameAt}
+          isFiltered={isFiltered}
+          removeGuestAt={removeGuestAt}
+          pendingGuest={pendingGuest} />
       </div>
     </div>
   );
